8a1613644bd1f1277fb2df08290978e769db94c5
Mvlprem <mvl.prem@outlook.com>
2024-03-29 13:32:02 +0530

    fix: express-rate-limit validate

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var app = express()
 // Set up rate limiter: maximum of twenty requests per minute
 const RateLimit = require('express-rate-limit')
 const limiter = RateLimit({
+  validate: { xForwardedForHeader: false },
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 60,
 })
